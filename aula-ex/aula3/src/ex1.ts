aa23f9aa27fbc6a702cf67a25a0181137c0c26cd
Lucas_Henrique_Vieira <lucasgamer2005com@gmail.com>
2024-11-01 20:02:28 -0300

    Resultado do ex.1 da aula 3

---
+++ aula-ex/aula3/src/ex1.ts
@@ -0,0 +1,30 @@
+//1 - Crie uma classe base "Veiculo" com os atributos marca e modelo, e um método detalhes. Crie uma classe derivada "Moto" 
+//que sobrescreva o método detalhes para incluir o número de cilindradas.
+
+class Veiculo{
+    protected placa: number;
+    protected cilindrada :number;
+  
+    constructor(placa:number,cilindrada:number){
+      this.cilindrada = cilindrada;
+      this.placa= placa;
+    }
+  
+    public detalhes(){
+      console.log(`Plca do veiculo é ${this.placa} com ${this.cilindrada}`)
+    }
+  }
+  class Moto extends Veiculo{
+
+    protected modelo:string;
+  
+    constructor(modelo:string,placa:number,cilindrada:number){
+      super(placa,cilindrada);
+      this.modelo = modelo;
+    }
+    
+    detalhes(){
+      super.detalhes();
+      console.log('sendo seu modelo'+this.modelo)
+    }
+  }
